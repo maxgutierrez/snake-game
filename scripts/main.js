f3ee665d9ac16e53ae3f4775b7e7bf85301d28fb
Max Gutierrez <35828336+maxgutierrez@users.noreply.github.com>
2020-12-11 03:02:36 -0200

    X

--- scripts/main.js
+++ scripts/main.js
@@ -5,32 +5,23 @@ window.onload = function() {
   const stage = document.querySelector('#stage')
   if (stage.getContext) {
     var ctx = stage.getContext('2d');
-  }
-  
+  }  
   document.addEventListener('keydown', keyPush)
- 
   setInterval(game, 100); // taxa de att do game()
   const vel = 1
-
   var vx = vy = 0 // vel xy
-
   var px = 10 // pos x
   var py = 10 // pos y
   var pr = 14
-
   var tp = 20 // tamanho peças (tabuleiro)
   var qp = 20 // quantidade de peças (tabuleiro)
   var ax = ay = 15 // apple xy
-  
   var trail = [] // = rastro
   tail = 5 // tamanho da cauda
-
-
 
   function game() {
     px += vx
     py += vy
-
     if (px < 0) {
       px = qp-1
     }
@@ -44,22 +35,14 @@ window.onload = function() {
       py = 0
     }
 
-    // ctx.fillStyle = 'green'
-    // ctx.fillRect(px*py)
-    
-
-
     ctx.fillStyle = 'black'// fundo
     ctx.fillRect(0,0, stage.width , stage.height)
-
-
 
     ctx.fillStyle =  ' rgb(7, 252, 7)' // maçã 
     ctx.fillRect(ax*tp , ay*tp, tp, tp)
 
 
-
-    for (var i = 0; i < trail.length; i++) {     
+    for (var i = 0; i < trail.length; i++) {// iterar o rastro
 
       ctx.beginPath()
       ctx.arc(trail[i].x*tp+10 , trail[i].y*tp+10, pr, 0, 2*Math.PI)
@@ -68,18 +51,13 @@ window.onload = function() {
       ctx.stroke()
       ctx.fillRect(trail[i].x*tp , trail[i].y*tp, tp, tp) 
 
-
+      var imagem = new Image();
+      imagem.src = "./imagem.png";
+      imagem.onload = function() {
+        ctx.drawImage(imagem, trail[trail.length-1].x*tp-5 , trail[trail.length-1].y*tp-5, 30, 30);
+      }      
       
-      
-
-      
-      // ctx.fillStyle = 'rgb(170, 110, 39)'
-      // ctx.fillRect(trail[trail.length-1].x*tp , trail[trail.length-1].y*tp, tp, tp)
-
-
-
-      
-      // ctx.beginPath();
+      // ctx.beginPath();// ROSTINHO DESENHADO
       // ctx.arc(trail[trail.length-1].x*tp+10 , trail[trail.length-1].y*tp+10, 10, 0, Math.PI * 2, true); // Círculo exterior
       // ctx.moveTo(trail[trail.length-1].x*tp+17 , trail[trail.length-1].y*tp+10);
       // ctx.arc(trail[trail.length-1].x*tp+10 , trail[trail.length-1].y*tp+10, 7, 0, Math.PI, false);  // Boca (sentido horário)
@@ -89,12 +67,8 @@ window.onload = function() {
       // ctx.arc(trail[trail.length-1].x*tp+15 , trail[trail.length-1].y*tp+7, 2, 0, Math.PI * 2, true);  // Olho direito
       // ctx.stroke()
 
-
-
-
-
-      if (trail[i].x == px && trail[i].y == py && tail != 5)//MORREU
-      {
+      //MORREU
+      if (trail[i].x == px && trail[i].y == py && tail != 5) {
         alert('POOOOXA!! VOCÊ MORREU KKK')
         vx = vy = 0
         px = py = 10
@@ -102,28 +76,19 @@ window.onload = function() {
       }      
     }
 
-    // ctx.fillStyle = 'rgb(224, 148, 49)'
-    // for (var i = 0; i < trail.length; i++) {      
-    //   ctx.fillRect(trail[trail.length-1].x*tp , trail[trail.length-1].y*tp, tp, tp)
-    //   // ctx.fillStyle =  'blue'
-    //   if (trail[0].x == px && trail[0].y == py)
-    //   {
-    //     vx = vy = 0
-    //     tail = 5
-    //   }      
-    // }
-    trail.push( { x:px , y:py } )
+    trail.push( { x:px , y:py } )// CAMINHAR
     while (trail.length > tail) {
       trail.shift()
     }
 
     if (ax==px && ay==py){ // se comer
       tail+= 2
-      ax = Math.floor(Math.random()* qp)
-      ay = Math.floor(Math.random()* qp)
+      const x = Math.floor(Math.random()* qp)
+      const y = Math.floor(Math.random()* qp)
+      ax = x
+      ay = y
     }
   }
-
 
   function keyPush(event) {
     switch (event.keyCode) {// →↓↑←
